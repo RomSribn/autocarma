2bc8941902c47588d7f10061ff5fc35ea852930b
RomSribn <steeper.fest@gmail.com>
2019-08-09 12:57:37 +0300

    valid routing

--- src/layout/components/Header/Header.jsx
+++ src/layout/components/Header/Header.jsx
@@ -1,38 +1,39 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import history from "../../../utils/history";
 import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import "./Header.css";
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    backgroundColor: "#efefef"
-  },
-  tabs: {
-    backgroundColor: "white"
-  }
-});
+const routes = [
+  "/",
+  "/accidents",
+  "/createAccident",
+  "/myAccidents",
+  "/about",
+  "/profile",
+  "/logout"
+];
 
 export default function CenteredTabs() {
-  const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
   function handleChange(event, newValue) {
+    history.push(routes[newValue]);
     setValue(newValue);
   }
 
   return (
-    <Paper className={classes.root}>
+    <Paper className="root">
       <Tabs
-        className={classes.tabs}
+        className="tabs"
         value={value}
         onChange={handleChange}
         indicatorColor="primary"
         textColor="primary"
         centered
       >
-        <Tab label="Autocarma" />
+        <Tab className="logo-bold" label="Autocarma" />
         <Tab label="Accidents" />
         <Tab label="Add new accidents" />
         <Tab label="My accidents" />
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,6 +6,12 @@ import Login from "./Login/index";
 const Router = () => (
   <Switch>
     <Route exact path="/" component={() => <div />} />
+    <Route exact path="/accidents" component={() => <div />} />
+    <Route exact path="/createAccident" component={() => <div />} />
+    <Route exact path="/myAccidents" component={() => <div />} />
+    <Route exact path="/about" component={() => <div />} />
+    <Route exact path="/profile" component={() => <div />} />
+    <Route exact path="/logout" component={() => <div />} />
   </Switch>
 );
 export default Router;
