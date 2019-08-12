bb5474d32ecec73dd526b92895865356b4317e60
RomSribn <steeper.fest@gmail.com>
2019-08-12 15:55:02 +0300

    eslint errors

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Login from "./Login/index";
 
 const Router = () => (
   <Switch>
