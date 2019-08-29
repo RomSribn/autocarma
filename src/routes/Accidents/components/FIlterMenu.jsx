6d5071fcb90fc8c111f9a4a2f2b8d9abef260df2
RomSribn <steeper.fest@gmail.com>
2019-08-29 19:43:08 +0300

    modal styles

--- src/routes/Accidents/components/FIlterMenu.jsx
+++ src/routes/Accidents/components/FIlterMenu.jsx
@@ -9,27 +9,49 @@ const SimpleSelect = () => {
   };
   return (
     <form>
+      <button type="button" className="license-button" onClick={toggle}>
+        Filtering
+      </button>
+
       <Drawer open={open} onRequestClose={toggle} modalElementClass="modal">
-        <div>Hey Im inside the drawer!</div>
+        <ul className="filter-menu">
+          <li className="select-wrap">
+            <label htmlFor="selectAccident">
+              Accident type:
+              <select
+                id="selectAccident"
+                className="form-item__element form-item__element--select"
+                required
+              >
+                <option disabled selected value="">
+                  Please select value
+                </option>
+                <option value="1">Parking law:</option>
+                <option value="2">Smth else:</option>
+              </select>
+            </label>
+          </li>
+          <li className="select-wrap">
+            <label htmlFor="selectAccident">
+              Time:
+              <select
+                id="selectAccident"
+                className="form-item__element form-item__element--select"
+                required
+              >
+                <option disabled selected value="">
+                  Please select value
+                </option>
+                <option value="1">Recently added</option>
+                <option value="2">For last month</option>
+                <option value="3">For last year</option>
+                <option value="4">Older</option>
+              </select>
+            </label>
+          </li>
+        </ul>
+        <input className="filter-submit" type="submit" value="Filter" onClick={toggle} />
       </Drawer>
-      <ul className="filter-menu">
-        <li>Filter by:</li>
-        <li className="select-wrap">
-          <select id="select" className="form-item__element form-item__element--select" required>
-            <option disabled selected value="">
-              Please select value
-            </option>
-            <option value="1">Accident type</option>
-            <option value="2">Time</option>
-          </select>
-        </li>
-        <li>
-          <div className="license-button">License plate</div>
-        </li>
-        <li>
-          <input className="filter-submit" type="submit" value="Filter" onClick={toggle} />
-        </li>
-      </ul>
     </form>
   );
 };
