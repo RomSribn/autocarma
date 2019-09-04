6981b1d8e9958dc044f38e18babf31c737a63c3f
RomSribn <steeper.fest@gmail.com>
2019-09-04 12:53:30 +0300

    mobile carousel (autocarma page)

---
+++ src/routes/Autocarma/components/MobileCarousel.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { MarkersProps } from 'types/index';
+import { useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import CarouselItem from './CarouselItem';
+
+interface IMobileCarouselProps {
+  markers: MarkersProps[];
+}
+
+const MobileCarousel = ({ markers }: IMobileCarouselProps) => {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps: number = markers.length;
+
+  function handleNext() {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  }
+
+  function handleStepChange(step) {
+    setActiveStep(step);
+  }
+
+  return (
+    <div className="mobile-carousel">
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {markers
+          ? markers.map(el => (
+            <Paper square elevation={0} className="mobile-carousel-item">
+              <CarouselItem key={el[0]} {...el[1]} />
+            </Paper>
+          ))
+          : null}
+      </SwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        variant="text"
+        activeStep={activeStep}
+        nextButton={(
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+            Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+)}
+        backButton={(
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+)}
+      />
+    </div>
+  );
+};
+
+export default MobileCarousel;
