7d99bb53f3175c7621dd3c9a4ecc5154391af2d5
RomSribn <steeper.fest@gmail.com>
2019-09-23 17:13:48 +0300

    valid pagination

--- src/routes/Accidents/components/Table.tsx
+++ src/routes/Accidents/components/Table.tsx
@@ -19,7 +19,12 @@ const SimpleTable = ({
   user,
 }: ISimpleTableProps) => {
   const [value, setValue] = React.useState(1);
-
+  const totalItemsCount = markers.length;
+  const itemsCountPerPage = 5;
+  const end = value * itemsCountPerPage;
+  const start = end - itemsCountPerPage;
+  const pgArray = markers.slice(start, end);
+  // debugger;
   const handleChange = (pageNumber) => {
     setValue(pageNumber);
   };
@@ -45,7 +50,7 @@ Accidents via @
               </TableRow>
             </TableHead>
             <TableBody>
-              {markers.map(el => (
+              {pgArray.map(el => (
                 <AccidentRow
                   gettingId={gettingId}
                   dumpingAccident={dumpingAccident}
@@ -61,8 +66,8 @@ Accidents via @
         <div className="pag-wrapper">
           <Pagination
             activePage={value}
-            itemsCountPerPage={10}
-            totalItemsCount={markers.length}
+            itemsCountPerPage={itemsCountPerPage}
+            totalItemsCount={totalItemsCount}
             pageRangeDisplayed={5}
             onChange={handleChange}
           />
