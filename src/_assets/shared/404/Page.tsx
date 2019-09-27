814f71980ccdf7ed15535115af967a98cc4ca0b3
RomSribn <steeper.fest@gmail.com>
2019-09-27 17:38:14 +0300

    redirect from 404 page

--- src/_assets/shared/404/Page.tsx
+++ src/_assets/shared/404/Page.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import history from 'utils/history';
+import { autocarma } from 'routes/variables';
 import './Page.scss';
 
 const NotFound = () => (
@@ -7,7 +9,9 @@ const NotFound = () => (
     <div className="error-page-title">
       <p>Sorry! Page not found</p>
     </div>
-    <div className="error-page-btn">Back to home</div>
+    <button className="error-page-btn" onClick={() => history.push(autocarma)}>
+      Back to home
+    </button>
   </div>
 );
 
